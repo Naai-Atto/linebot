4c50d8c87d850259ac6a8e03c0cf70030c5cedde
Naai-Atto <102561617+Naai-Atto@users.noreply.github.com>
2022-03-29 07:32:19 +0700

    Update line.php

--- line.php
+++ line.php
@@ -2,7 +2,7 @@
   
 
 function send_LINE($msg){
- $access_token = '#ใส่ TOKEN HERE'; 
+ $access_token = 'BvD4wWX3vFNMy1QOZrd/onyCNGvC9mQzQRzuwc2dmzMG42uKu/CCTQKRByc3CBL7OovAmlaQsS4foDweQV9E5Hswb//YE7b0o60Ka+MZ0R/6ZJLrrTQUqBLISIeip9UYLwQce7MYQ8uoG+fy9/3sGAdB04t89/1O/w1cDnyilFU='; 
 
   $messages = [
         'type' => 'text',
@@ -14,7 +14,7 @@ function send_LINE($msg){
       $url = 'https://api.line.me/v2/bot/message/push';
       $data = [
 
-        'to' => '#ใส่ ID HERE',
+        'to' => 'Ubbc7673c0909df9495237b42aed327bb',
         'messages' => [$messages],
       ];
       $post = json_encode($data);
